3d2138b55de20a2c331cbeb782c5ccccbcab8b97
Ben <cbtsao47@gmail.com>
2019-07-13 16:51:25 -0500

    setup guitar base class

---
+++ question2.js
@@ -0,0 +1,30 @@
+// Represent a 6 string guitar (standard tuning, only the first 2 fret boards) and a 4 string bass
+// (standard tuning, only the first 3 frets) in a class structure. Create attributes, elements, functions
+// and any other components you feel they may have.
+// 1. Consider where they are similar and where they are different.
+// 2. Consider we want to identify if the note from one is equal to the other based on the fret
+// position and string played.
+
+// Please note this doesn’t need to be an executable application , only a collection of classes that
+// would represent the guitar , the bass in its commonality and its differences.
+
+class Guitar {
+  constructor(strings = ["E", "B", "G", "D", "A", "E"], fret = [0, 1, 2]) {
+    this.strings = strings;
+    this.fret = fret;
+    this.notes = {
+      0: this.strings,
+      1: ["F", "C", "G#", "D#", "A#", "F"],
+      2: ["F#", "C#", "A", "E", "B", "F#"]
+    };
+  }
+  playNote(string, fret = 0) {
+    //   if fret is not provided, defaults to 0 (open string)
+    let stringIndex = string - 1;
+    let isPartOfStrings = this.strings.length >= string;
+    if (string && isPartOfStrings) {
+      return `Note played: ${this.notes[fret][stringIndex]}`;
+    }
+    return `Please enter correct format! string(1-6),[fret(0-2)] 0 means open string`;
+  }
+}
