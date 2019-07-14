9c80481f524b0711c3859b817775b812cc8f7459
Ben <cbtsao47@gmail.com>
2019-07-13 23:08:07 -0500

    question4 base

--- question4/src/App.js
+++ question4/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// components
+import TextBox from "./components/TextBox/TextBox";
+import Button from "./components/Buttons/Button";
+
+class App extends React.Component {
+  state = {
+    userInput: ""
+  };
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+  countWords = (
+    userInput = "how are you?\nI'm good!",
+    vowelCount,
+    lineCount = 1
+  ) => {
+    let sentences = userInput.split("\n");
+    let inputArr = [];
+    // separate the user input by lines
+    for (let i = lineCount - 1; i < sentences.length; i = i + lineCount) {
+      let currentLine = {};
+      let sentence = sentences[i].split(" ");
+      let count = 0;
+      let wordsArr = sentence.filter((word, i) => (i + 1) % 3 === 0);
+
+      inputArr.push(sentences[i]);
+    }
+  };
+  render() {
+    const { userInput } = this.state;
+    const resultInput = this.countWords();
+    return (
+      <div className="App">
+        <TextBox userInput={userInput} handleChange={this.handleChange} />
+        <Button />
+        <TextBox userInput={resultInput} disabled />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ question4/src/components/TextBox/TextBox.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const TextBox = ({ userInput, handleChange, disabled }) => {
+  return (
+    <form action="">
+      <textarea
+        name="userInput"
+        id=""
+        cols="30"
+        rows="10"
+        value={userInput}
+        onChange={handleChange}
+        disabled={disabled}
+      />
+    </form>
+  );
+};
+
+export default TextBox;
